cd1f1ff914cb3f3ac6d964d5cec52b122e906e29
grisha18 <granta54321@gmail.com>
2021-12-18 11:56:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const productNameRowButton = document.querySelector('.product-name-row__button button');
+const formBlockTwo = document.querySelector('.form-block__two');
+
+productNameRowButton.addEventListener('click', ()=>{
+
+        console.log(111);
+        formBlockTwo.scrollIntoView();
+
+})
+
+
+
